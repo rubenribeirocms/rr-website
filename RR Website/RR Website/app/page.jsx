bdd5b52cf4e516dec1bb5fee25f3263e5f78a5c1
rubenribeirocms <ruben@rrbeauty.co.uk>
2025-11-25 15:21:50 +0000

    Create page.jsx

---
+++ RR Website/RR Website/app/page.jsx
@@ -0,0 +1,10 @@
+export default function HomePage() {
+  return (
+    <div style={{ padding: '40px', fontFamily: 'Arial' }}>
+      <h1 style={{ fontSize: '32px', marginBottom: '20px' }}>
+        RR Website is Live
+      </h1>
+      <p>This is your homepage. Replace this with your real design.</p>
+    </div>
+  );
+}
